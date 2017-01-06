072436d9e06f95374fbd0a98e2eb41ee66ad9362
Brandon Soto <brandon.soto09@gmail.com>
2017-01-05 23:47:51 -0800

    Run code through standard command

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  'use strict' // Start of use strict
 
-    var box = $('#box');
-    var offset = box.offset();
-    var center_x = (offset.left) + (box.width() / 2);
-    var center_y = (offset.top) + (box.height() / 2);
+  var box = $('#box')
+  var offset = box.offset()
+  var center_x = (offset.left) + (box.width() / 2)
+  var center_y = (offset.top) + (box.height() / 2)
 
-    function mousemoved(event) {
-        var event_x = event.pageX;
-        var event_y = event.pageY;
-        var radians = Math.atan2(event_x - center_x, event_y - center_y);
-        var degree = (radians * (180 / Math.PI) * -1) + 90;
+  function mousemoved (event) {
+    var event_x = event.pageX
+    var event_y = event.pageY
+    var radians = Math.atan2(event_x - center_x, event_y - center_y)
+    var degree = (radians * (180 / Math.PI) * -1) + 90
 
-        box.css('-moz-transform', 'rotate(' + degree + 'deg)');
-        box.css('-webkit-transform', 'rotate(' + degree + 'deg)');
-        box.css('-o-transform', 'rotate(' + degree + 'deg)');
-        box.css('-ms-transform', 'rotate(' + degree + 'deg)');
+    box.css('-moz-transform', 'rotate(' + degree + 'deg)')
+    box.css('-webkit-transform', 'rotate(' + degree + 'deg)')
+    box.css('-o-transform', 'rotate(' + degree + 'deg)')
+    box.css('-ms-transform', 'rotate(' + degree + 'deg)')
 
-        var pageCoords = event.pageX + ", " + event.pageY;
-        var clientCoords = event.clientX + ", " + event.clientY;
-        $('#coords').text("( pageX, pageY ) : " + pageCoords +
-            " ( clientX, clientY ) : " + clientCoords);
-    }
+    var pageCoords = event.pageX + ', ' + event.pageY
+    var clientCoords = event.clientX + ', ' + event.clientY
+    $('#coords').text('( pageX, pageY ) : ' + pageCoords +
+            ' ( clientX, clientY ) : ' + clientCoords)
+  }
 
     // only moves the box when the mouse is held down
     // $('#test-body').mousedown(function(){
@@ -32,6 +32,6 @@
     //     $(document).unbind();
     // });
 
-    $( document ).on( "vmousemove", mousemoved );
-    $.mobile.loading().hide();
-})(jQuery); // End of use strict
+  $(document).on('vmousemove', mousemoved)
+  $.mobile.loading().hide()
+})(jQuery) // End of use strict
