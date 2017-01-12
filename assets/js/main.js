e37765d80e019c097ab6c79d8cb03ddf6113de0b
Brandon Soto <brandon.soto09@gmail.com>
2017-01-11 22:30:23 -0800

    Write initial implementations of fire functions & reorganize divs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,53 @@
 $(function () {
-  'use strict'; // Start of use strict
+    'use strict'; // Start of use strict
 
-  var box = $('#box');
-  var offset = box.offset();
-  var center_x = (offset.left) + (box.width() / 2);
-  var center_y = (offset.top) + (box.height() / 2);
+    var main_container = $('#main-container');
+    var cannon = $('.cannon');
+    var click_count = 1;
 
-  function mousemoved (event) {
-    var event_x = event.pageX;
-    var event_y = event.pageY;
-    var radians = Math.atan2(event_x - center_x, event_y - center_y);
-    var degree = (radians * (180 / Math.PI) * -1) + 90;
+    function fire_cannon(fire_event) {
+        main_container.append('<div class="cannon-ball"></div>');
 
-    box.css('-moz-transform', 'rotate(' + degree + 'deg)');
-    box.css('-webkit-transform', 'rotate(' + degree + 'deg)');
-    box.css('-o-transform', 'rotate(' + degree + 'deg)');
-    box.css('-ms-transform', 'rotate(' + degree + 'deg)');
+        var cannon_ball = $('.cannon-ball');
+        var cannon_offset = cannon.offset();
 
-    var pageCoords = event.pageX + ', ' + event.pageY;
-    var clientCoords = event.clientX + ', ' + event.clientY;
-    $('#coords').text('( pageX, pageY ) : ' + pageCoords +
-            ' ( clientX, clientY ) : ' + clientCoords);
-  }
+        cannon_ball.fadeIn(200);
+        cannon_ball.offset({top: cannon_offset.top, left: cannon_offset.left});
+        cannon_ball.fadeOut(200);
+
+        console.log('cannon fired! ' + click_count++);
+        main_container.remove('.cannon-ball');
+    }
+
+    function mousemoved(event) {
+        var cannon_offset = cannon.offset();
+        var center_x = (cannon_offset.left) + (cannon.width() / 2);
+        var center_y = (cannon_offset.top) + (cannon.height() / 2);
+
+        var event_x = event.pageX;
+        var event_y = event.pageY;
+        var radians = Math.atan2(event_x - center_x, event_y - center_y);
+        var degree = (radians * (180 / Math.PI) * -1) + 90;
+
+        cannon.css('-moz-transform', 'rotate(' + degree + 'deg)');
+        cannon.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+        cannon.css('-o-transform', 'rotate(' + degree + 'deg)');
+        cannon.css('-ms-transform', 'rotate(' + degree + 'deg)');
+
+        var pageCoords = event.pageX + ', ' + event.pageY;
+        var clientCoords = event.clientX + ', ' + event.clientY;
+        $('#coords').text('( pageX, pageY ) : ' + pageCoords
+            + ' ( clientX, clientY ) : ' + clientCoords);
+    }
 
     // only moves the box when the mouse is held down
-    // $('#test-body').mousedown(function(){
-    //     console.log('logged mouse down');
-    //     $(document).mousemove(mousemoved);
-    // }).mouseup(function(){
-    //     console.log('logged mouse up');
-    //     $(document).unbind();
-    // });
+    $('html').mousedown(function () {
+        $(document).mousemove(mousemoved);
+    }).mouseup(function () {
+        $(document).unbind();
+    });
 
-  $(document).on('vmousemove', mousemoved);
-  $.mobile.loading().hide();
-
+    // $(document).on('vmousemove', mousemoved);
+    $('#fire').on('click', function(event) { fire_cannon(event) } );
+    $.mobile.loading().hide();
 }); // end of document ready
