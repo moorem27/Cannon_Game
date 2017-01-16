0038dea0060d9fed6ce306f3a9b7c7faf9953f34
Brandon Soto <brandon.soto09@gmail.com>
2017-01-15 20:50:13 -0800

    Remove browser specific transformations.
    As of jQuery 1.8, browser specific transformations will be
    added automatically. No need to declare them ourselves.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,12 +36,9 @@ $(function () {
         var event_x = event.pageX;
         var event_y = event.pageY;
         var radians = Math.atan2( event_y - center_y, event_x - center_x);
-        var degree = (radians * (180 / Math.PI));
+        var degrees = (radians * (180 / Math.PI));
 
-        cannon.css('-moz-transform', 'rotate(' + degree + 'deg)');
-        cannon.css('-webkit-transform', 'rotate(' + degree + 'deg)');
-        cannon.css('-o-transform', 'rotate(' + degree + 'deg)');
-        cannon.css('-ms-transform', 'rotate(' + degree + 'deg)');
+        cannon.css({'transform' : 'rotate(' + degrees + 'deg)'});
 
         var pageCoords = event.pageX + ', ' + event.pageY;
         var clientCoords = event.clientX + ', ' + event.clientY;
